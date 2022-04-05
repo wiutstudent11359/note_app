f0c17a74e7349d1a21b700d19d46961d8e076107
wiutstudent11359 <wiutstudent11359@gmail.com>
2022-04-05 23:06:15 +0500

    commit

--- app.js
+++ app.js
@@ -21,17 +21,42 @@ app.get('/add', (req, res) => {
 app.post('/add', (req, res) => {
     const subject = req.body.subject
     const text = req.body.text
-    if (title.trim() === '' && text.trim() === '') {
+    if (subject.trim() === '' && text.trim() === '') {
         res.render('add', { error: true })
+    } else {
+        fs.readFile('./data/notes.json', (err, data) => { 
+            if (err) throw err
+
+            const notes = JSON.parse(data)
+
+            notes.push({
+                id: id (),
+                subject: subject,
+                text: text,
+            })
+        fs.writeFile('./data/notes.json', JSON.stringify(notes), err => {
+            if (err) throw err
+
+            res.render('add', { success: true })
+        })
+        })
     }
 })
-const notes = ['there was cringe subject', 'there was cringe subject 2']
+const notes = ['this is cringe subject', 'this is cringe subject 2']
 
 app.get('/notes', (req, res) => {
-    res.render ('notes', { notes : notes })
+    fs.readFile('./data/notes.json', (err, data) => {
+        if (err) throw err
+
+        const notes = JSON.parse(data)
+
+        res.render ('notes', { notes : notes })
+    })
+    
 })
 
-app.get('/notes/detail', (req, res) => {
+app.get('/notes/:id', (req, res) => {
+    const id = req.params.id
     res.render ('detail')
 })
 app.listen(8000, err => {
@@ -39,3 +64,7 @@ app.listen(8000, err => {
 
     console.log('Server is running on port 8000')
 })
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  }
