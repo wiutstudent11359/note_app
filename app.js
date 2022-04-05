a33b6b6633636dc37c012653b6d3e5e7067cdeb1
wiutstudent11359 <wiutstudent11359@gmail.com>
2022-04-05 22:25:06 +0500

    commit

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 
+const fs = require('fs')
+const { title } = require('process')
+
 app.set('view engine', 'pug')
 
 app.use('/static', express.static('public')) //assets
+app.use(express.urlencoded({ extended: false }))
 
 //localhost:8000
 app.get('/', (req, res) => {
@@ -14,6 +18,22 @@ app.get('/add', (req, res) => {
     res.render ('add')
 })
 
+app.post('/add', (req, res) => {
+    const subject = req.body.subject
+    const text = req.body.text
+    if (title.trim() === '' && text.trim() === '') {
+        res.render('add', { error: true })
+    }
+})
+const notes = ['there was cringe subject', 'there was cringe subject 2']
+
+app.get('/notes', (req, res) => {
+    res.render ('notes', { notes : notes })
+})
+
+app.get('/notes/detail', (req, res) => {
+    res.render ('detail')
+})
 app.listen(8000, err => {
     if (err) console.log(err)
 
